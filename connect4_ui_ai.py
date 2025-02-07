76e8249f9c06239d5a3769af5a802ee52d29aa94
Choollol <67297627+Choollol@users.noreply.github.com>
2025-02-07 11:48:01 -0800

    Function organization and lookahead moves change

--- connect4_ui_ai.py
+++ connect4_ui_ai.py
@@ -18,7 +18,7 @@ POP = 2
 PLAYER_COLOR = connect4.RED
 AI_COLOR = connect4.YELLOW
 
-LOOKAHEAD_MOVE_THRESHOLDS = [(5, 6), (8, 5), (12, 4), (20, 4)]
+LOOKAHEAD_MOVE_THRESHOLDS = [(8, 5), (12, 4), (20, 3)]
 DEFAULT_MAX_SCORE = -1000000000
 
 LOOKAHEAD_EDGE_RADIUS = 2
@@ -59,7 +59,7 @@ def faster_win_check(game_state: connect4.GameState, columnCount: int, rowCount:
 
     for col in range(columnCount):
         for row in range(rowCount):
-            if _winning_sequence_begins_at(game_state.board, col, row, columnCount, rowCount):
+            if game_state.board[col][row] != connect4.EMPTY and _winning_sequence_begins_at(game_state.board, col, row, columnCount, rowCount):
                 if winner == connect4.EMPTY:
                     winner = game_state.board[col][row]
                 elif winner != game_state.board[col][row]:
@@ -112,7 +112,7 @@ def simulate_move(gameState: connect4.GameState, move: int, movesRemaining: int,
         #if connect4.winner(front[0]) != connect4.EMPTY:
         if faster_win_check(front[0], column_count, row_count) != connect4.EMPTY:
             #total_score += get_win_score(front[0]) * front[2] ** front[2]
-            total_score += get_single_score(connect4.winner(front[0])) * front[2] ** front[2]
+            total_score += get_single_score(connect4.winner(front[0])) * front[2] ** 2
             continue
         if front[1] == DROP:
             if not is_move_valid(front[0], DROP, front[3]):
@@ -145,6 +145,79 @@ def simulate_move_helper(gameState: connect4.GameState, move: int, movesRemainin
     scores[column] = simulate_move(
         gameState, move, movesRemaining, column, lookaheadRange)
 
+def multithreaded_move(gameState: connect4.GameState, lookaheadRange, lookahead_moves: int, lookaheadRangeLeft: int, lookaheadRangeRight: int) -> tuple[int, int]:
+    max_score = DEFAULT_MAX_SCORE
+    
+    manager = multiprocessing.Manager()
+    scores = manager.dict()
+    processes: list[multiprocessing.Process] = []
+    for col in lookaheadRange:
+        if not is_move_valid(gameState, DROP, col):
+            continue
+        process = multiprocessing.Process(target=simulate_move_helper, args=(
+            gameState, DROP, lookahead_moves, col, lookaheadRange, scores))
+        process.start()
+        processes.append(process)
+
+    for process in processes:
+        process.join()
+
+    possible_columns = [random.randint(lookaheadRangeLeft + LOOKAHEAD_EDGE_RADIUS,
+                                       lookaheadRangeRight - LOOKAHEAD_EDGE_RADIUS + 1)]
+    
+    found_nonzero_score = False
+    for col, score in scores.items():
+        print(F"Column {col}'s score: {score}")
+        if score != 0:
+            found_nonzero_score = True
+        if score > max_score:
+            max_score = score
+            # column = col
+            possible_columns.clear()
+            possible_columns.append(col)
+        elif score == max_score:
+            possible_columns.append(col)
+    
+    possible_columns.sort()
+    column = possible_columns[len(possible_columns) // 2]
+
+    if not found_nonzero_score:
+        column = random.randint(lookaheadRangeLeft, lookaheadRangeRight + 1)
+        
+    return (DROP, column)
+
+
+def singlethreaded_move(gameState: connect4.GameState, lookaheadRange, lookahead_moves: int, lookaheadRangeLeft: int, lookaheadRangeRight: int) -> tuple[int, int]:
+    max_score = DEFAULT_MAX_SCORE
+    
+    possible_columns = [random.randint(lookaheadRangeLeft + LOOKAHEAD_EDGE_RADIUS,
+                                       lookaheadRangeRight - LOOKAHEAD_EDGE_RADIUS + 1)]
+    #column = random.randint(lookaheadRangeLeft + LOOKAHEAD_EDGE_RADIUS, lookaheadRangeRight - LOOKAHEAD_EDGE_RADIUS + 1)
+    found_nonzero_score = False
+    for col in lookaheadRange:
+        if not is_move_valid(gameState, DROP, col):
+            continue
+        score = simulate_move(
+            gameState, DROP, lookahead_moves, col, lookaheadRange)
+        print(f"Column {col}'s score: {score}")
+        if score != 0:
+            found_nonzero_score = True
+        if score > max_score:
+            max_score = score
+            #column = col
+            possible_columns.clear()
+            possible_columns.append(col)
+        elif score == max_score:
+            possible_columns.append(col)
+    
+    column = possible_columns[len(possible_columns) // 2]
+    
+    if not found_nonzero_score:
+        column = random.randint(lookaheadRangeLeft + LOOKAHEAD_EDGE_RADIUS,
+                                lookaheadRangeRight - LOOKAHEAD_EDGE_RADIUS + 1)
+    
+    return (DROP, column)
+
 def ai_move(gameState: connect4.GameState) -> tuple[int, int]:
     '''
     Asks the AI to choose a move, returning a tuple whose first element
@@ -159,73 +232,31 @@ def ai_move(gameState: connect4.GameState) -> tuple[int, int]:
 
     column_count = connect4.columns(gameState)
 
-    for pair in LOOKAHEAD_MOVE_THRESHOLDS:
-        if column_count <= pair[0]:
-            lookahead_moves = pair[1]
-            break
-
-    column = random.randint(1, column_count)
-    max_score = DEFAULT_MAX_SCORE
-    
+    # Get lookahead range
     lookaheadRangeLeft = column_count // 2 - LOOKAHEAD_EDGE_RADIUS
     lookaheadRangeRight = column_count // 2 + LOOKAHEAD_EDGE_RADIUS
-    
+
     for col in range(1, column_count + 1):
         if gameState.board[col - 1][-1] != connect4.EMPTY:
-            lookaheadRangeLeft = col - LOOKAHEAD_EDGE_RADIUS if col - LOOKAHEAD_EDGE_RADIUS >= 1 else 1
+            lookaheadRangeLeft = col - LOOKAHEAD_EDGE_RADIUS if col - \
+                LOOKAHEAD_EDGE_RADIUS >= 1 else 1
             break
-            
+
     for col in range(column_count, 0, -1):
         if gameState.board[col - 1][-1] != connect4.EMPTY:
-            lookaheadRangeRight = col + LOOKAHEAD_EDGE_RADIUS if col + LOOKAHEAD_EDGE_RADIUS <= column_count else column_count
+            lookaheadRangeRight = col + LOOKAHEAD_EDGE_RADIUS if col + \
+                LOOKAHEAD_EDGE_RADIUS <= column_count else column_count
             break
-    
+
     lookaheadRange = range(lookaheadRangeLeft, lookaheadRangeRight + 1)
 
-    manager = multiprocessing.Manager()
-    scores = manager.dict()
-    processes: list[multiprocessing.Process] = []
-    for col in lookaheadRange:
-        if not is_move_valid(gameState, DROP, col):
-            continue
-        process = multiprocessing.Process(target=simulate_move_helper, args=(
-            gameState, DROP, lookahead_moves, col, lookaheadRange, scores))
-        process.start()
-        processes.append(process)
-        
-    for process in processes:
-        process.join()
-        
-    found_nonzero_score = False
-    for col, score in scores.items():
-        print(F"Column {col}'s score: {score}")
-        if score != 0:
-            found_nonzero_score = True
-        if score > max_score:
-            max_score = score
-            column = col
-            
-    if not found_nonzero_score:
-        column = random.randint(lookaheadRangeLeft, lookaheadRangeRight + 1)
-
-                
-    """ for col in range(1, column_count + 1):
-        if scores[col - 1] == DEFAULT_MAX_SCORE:
-            continue
-        score = scores[col - 1]
-        print(F"Column {col}'s score: {score}")
-        if score > max_score:
-            max_score = score
-            column = col """
+    # Get lookahead moves based on lookahead range
+    """ for pair in LOOKAHEAD_MOVE_THRESHOLDS:
+        if lookaheadRangeRight - lookaheadRangeLeft + 1 <= pair[0]:
+            lookahead_moves = pair[1]
+            break """
     
-    """ for col in range(1, column_count + 1):
-        if not is_move_valid(gameState, DROP, col):
-            continue
-        score = simulate_move(gameState, DROP, lookahead_moves, col, column_count + 1)
-        print(f"Column {col}'s score: {score}")
-        if score != 0 and score > max_score:
-            max_score = score
-            column = col  """
-
-    # print(f"Chosen column: {column}")
-    return (DROP, column)
+    # Set lookahead moves to 3 because apparently that's all it needs
+    lookahead_moves = 3
+    return multithreaded_move(gameState, lookaheadRange, lookahead_moves, lookaheadRangeLeft, lookaheadRangeRight)
+    #return singlethreaded_move(gameState, lookaheadRange, lookahead_moves, lookaheadRangeLeft, lookaheadRangeRight)
